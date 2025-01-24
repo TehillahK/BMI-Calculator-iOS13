d684767a0e7e3cedc62456728a04d04ebe1c6d7f
Tehillahk <kangambt@myumanitoba.ca>
2025-01-23 22:41:31 -0600

    slider values reflect in labels

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightValLabel: UILabel!
+    
+    @IBOutlet weak var weightValLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        
+        let maxVal = sender.maximumValue
+        
+        let sliderValue = sender.value
+        
+        // determines which slider's value changed
+        if(maxVal == 3.0){
+            
+            // height label slided
+            let userHeight = String(format:"%.2f",sliderValue)
+            heightValLabel.text = "\(userHeight)m"
+            
+        }else{
+            
+            // weight label slided
+            let userWeight = String(format: "%.0f", sliderValue)
+            weightValLabel.text = "\(userWeight)Kg"
+        }
+       
+        
+        
+        
+    }
+    
 }
 
